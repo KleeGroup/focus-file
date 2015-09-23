e37b793bbd5c0c64915b2a2419e5cf5ddcffd4b7
Thomas <tgallon@gmail.com>
2015-09-23 19:58:28 +0200

    [dropZone] refacto + first try of redefying template

--- src/component/index.js
+++ src/component/index.js
@@ -1,46 +1,52 @@
 //Dependencies
-import React, { Component } from 'react';
-//import accountStoreInstance from './built-in-store';
-//import {load} from './action/account';
+import React, {Component, PropTypes} from 'react';
 import Dropzone from 'dropzone';
 import uuid from 'uuid';
-import {component} from 'focus';
-let type = component.types;
+
+const propTypes = {
+    withCredentials: PropTypes.bool,
+    style: PropTypes.object,
+    url: PropTypes.string,
+    previewTemplate: PropTypes.string
+};
+
+const defaultProps = {
+    withCredentials: true,
+    previewTemplate: '<div>test</div>'
+};
+
 /**
- * Component use for uploading files.
- */
-export default class FileUploadZone extends Component {
-  /** @inheritdoc */
-  constructor(props) {
-      super(props);
-      this.displayName = 'FileUploadZone';
-      this.fileId = uuid.v4();
-  }
-  /** @inheritdoc */
-  componentDidMount(){
-      this.dropzone = new Dropzone(`div[data-file-upload='${this.fileId}']`, this.props);
-  }
-  /** @inheritdoc */
-  getDefaultProps(){
-      return {
-          withCredentials: true
-      };
-  }
-  /** @inheritdoc */
-  propTypes(){
-      return {
-          withCredentials: type('bool'),
-          style: type('object')
-      };
-  }
-  /** @inheritdoc */
-  render() {
-      let {fileId} = this;
-      let {style} = this.props;
-      return (
-          <div data-focus='file-upload' className='dropzone dz-clickable'>
+* Component use for uploading files.
+*/
+class FileUploadZone extends Component {
+    constructor(props) {
+        super(props);
+        const state = {
+            fileId: uuid.v4()
+        };
+        this.state = state;
+    }
+
+    /** @inheritdoc */
+    componentDidMount(){
+        const {fileId} = this.state;
+        const {template} = this.props;
+        this.dropzone = new Dropzone(`div[data-file-upload='${fileId}']`, this.props);
+    }
+    /** @inheritdoc */
+    render() {
+        const {style} = this.props;
+        const {fileId} = this.state;
+        return (
+            <div data-focus='file-upload' className='dropzone dz-clickable'>
                 <div data-file-upload={fileId} style={style}></div>
             </div>
-      );
-  }
+        );
+    }
 }
+
+FileUploadZone.displayName = 'FileUploadZone';
+FileUploadZone.defaultProps = defaultProps;
+FileUploadZone.propTypes = propTypes;
+
+export default FileUploadZone;
