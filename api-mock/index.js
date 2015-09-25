28892ec5e25b56d7200d1d7e8d6c06bb8fdc5d2c
Stan <bernardstanislas@gmail.com>
2015-09-25 17:09:16 +0200

    [api] Fix mock

--- api-mock/index.js
+++ api-mock/index.js
@@ -1,32 +1,32 @@
-var express = require('express'),
-    multer  = require('multer'),
-    done = false;
+const express = require('express');
 
-var app = express();
+const app = express();
 
 //CORS middleware
-var allowCrossDomain = function(req, res, next) {
-  res.header("Access-Control-Allow-Origin", "*");
-  res.header("Access-Control-Allow-Headers", "X-Requested-With,Content-Type,Cache-Control");
-  res.header('Content-Type', 'application/json');
-  next();
-}
+const allowCrossDomain = (req, res, next) => {
+    res.header('Access-Control-Allow-Origin', 'http://localhost:3000');
+    res.header('Access-Control-Allow-Credentials', 'true');
+    res.header('Access-Control-Allow-Headers', 'X-Requested-With,Content-Type,Cache-Control');
+    res.header('Content-Type', 'application/json');
+    next();
+};
 app.use(allowCrossDomain);
-var fileMiddleWareConfig = require('./file-middleware');
-app.use(new fileMiddleWareConfig().init());
 
-app.get('/', function(req, res){
-  res.send('Functional file server');
+const FileMiddleWareConfig = require('./file-middleware');
+app.use(new FileMiddleWareConfig().init());
+
+app.get('/', (req, res) => {
+    res.send('Functional file server');
 });
-app.post('/upload', function (req, res) {
-    res.sendStatus(200);
+
+app.post('/upload', (req, res) => {
+    console.log(req);
+    // console.log(req.body); // form fields
+    // console.log(req.files); // form files
+    res.status(204).end();
 });
-app.post('/upload', function(req, res, next){
-    console.log(req.body) // form fields
-    console.log(req.files) // form files
-    res.status(204).end()
-})
-app.listen(9998,function() {
- var port = 9998;
- console.log('File Mocked API listening at http://localhost:%s', port);
+
+app.listen(9998, () => {
+    const port = 9998;
+    console.log('File Mocked API listening at http://localhost:%s', port);
 } );
