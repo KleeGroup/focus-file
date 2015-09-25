694f68e1c9411a743a66f512e80cb5e8ea492a8a
Stan <bernardstanislas@gmail.com>
2015-09-25 17:12:49 +0200

    [component] Working

--- src/component/index.js
+++ src/component/index.js
@@ -1,18 +1,29 @@
 //Dependencies
 import React, {Component, PropTypes} from 'react';
 import Dropzone from 'dropzone';
+import 'dropzone/dist/dropzone.css';
+import './style/index.scss';
+import builtInStore from '../store/built-in-store';
 import uuid from 'uuid';
+import {dispatcher} from 'focus-core';
 
 const propTypes = {
     withCredentials: PropTypes.bool,
     style: PropTypes.object,
     url: PropTypes.string,
-    previewTemplate: PropTypes.string
+    paramName: PropTypes.string,
+    previewTemplate: PropTypes.string,
+    removalTimeout: PropTypes.number,
+    store: PropTypes.object
 };
 
 const defaultProps = {
-    withCredentials: true,
-    previewTemplate: '<div>test</div>'
+    paramName: 'upfile',
+    removalTimeout: 1500,
+    headers: {
+        'Cache-Control': null
+    },
+    store: builtInStore
 };
 
 /**
@@ -22,29 +33,43 @@ class FileUploadZone extends Component {
     constructor(props) {
         super(props);
         const state = {
+            dragging: false,
             fileId: uuid.v4()
         };
         this.state = state;
     }
 
     /**
-     * Component did mount
-     */
+    * Component did mount
+    */
     componentDidMount(){
         const {fileId} = this.state;
-        // const {template} = this.props;
         this.dropzone = new Dropzone(`div[data-file-upload='${fileId}']`, this.props);
+        this.dropzone.on('complete', this._onFileComplete);
     }
+
+    _onFileComplete = file => {
+        const {removalTimeout, store} = this.props;
+        setTimeout(() => {
+            this.dropzone.removeFile(file);
+        }, removalTimeout);
+        const files = store.getFiles() || [];
+        files.push(file);
+        dispatcher.handleServerAction({
+            data: {files},
+            type: 'update'
+        });
+    }
+
     /**
-     * Render the component
-     * @return {JSX} The rendered component
-     */
+    * Render the component
+    * @return {JSX} The rendered component
+    */
     render() {
-        const {style} = this.props;
         const {fileId} = this.state;
         return (
             <div className='dropzone dz-clickable' data-focus='file-upload'>
-                <div data-file-upload={fileId} style={style}></div>
+                <div data-file-upload={fileId} data-focus='file-upload-dropzone'></div>
             </div>
         );
     }
