a28683823d9fa142a163db41d81980d296e1f53a
Stan <bernardstanislas@gmail.com>
2015-09-25 17:11:05 +0200

    [example] Move files

--- src/action/file.js
+++
@@ -1,8 +0,0 @@
-import {loadFileFromUrl, saveFile} from '../service/file';
-import { application } from 'focus';
-let actionBuilder = application.actionBuilder;
-export const save = actionBuilder({
-    service: loadFileFromUrl,
-    node: 'account',
-    status: 'loaded'
-});
